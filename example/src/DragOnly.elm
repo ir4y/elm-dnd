c7ab19d9d28ba6169ce5fc5f8e9e850d28004372
Ilya Beda <ir4y.ix@gmail.com>
2016-11-11 22:19:30 +0700

    Add simple example

---
+++ example/src/DragOnly.elm
@@ -0,0 +1,55 @@
+module Box exposing (..)
+
+import Html.App as Html
+import Html.Attributes exposing (style)
+import Html exposing (..)
+import DnD
+
+
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ DnD.subscriptions DnDMsg model
+        ]
+
+
+type alias Model =
+    DnD.Draggable String Msg
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( DnD.init, Cmd.none )
+
+
+type Msg
+    = DnDMsg (DnD.Msg String Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd.Cmd Msg )
+update (DnDMsg msg) model =
+    ( DnD.update msg model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ DnD.draggable DnDMsg "drag-n-drop" [] [ text "hello" ]
+        , DnD.dragged
+            model
+            dragged
+        ]
+
+
+dragged : String -> Html Msg
+dragged item =
+    div [] [ text item ]
